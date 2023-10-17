f41a06bba0e51f28c5d0cc44b1a83a207a19d7f6
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-10-18 00:46:26 +0530

    init

--- App.tsx
+++ App.tsx
@@ -5,16 +5,17 @@ import {
   rainbowWallet,
   ThirdwebProvider,
 } from '@thirdweb-dev/react-native';
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {StyleSheet, Text, useColorScheme, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {TW_CLIENT_ID} from '@env';
+import { nfcManager } from 'react-native-nfc-manager';
 
 const App = () => {
   return (
     <ThirdwebProvider
       activeChain="mumbai"
-      // clientId={TW_CLIENT_ID} // uncomment this line after you set your clientId in the .env file
+      clientId={TW_CLIENT_ID} // uncomment this line after you set your clientId in the .env file
       supportedWallets={[metamaskWallet(), rainbowWallet(), localWallet()]}>
       <AppInner />
     </ThirdwebProvider>
@@ -29,12 +30,50 @@ const AppInner = () => {
     ...styles.heading,
   };
 
-  return (
-    <View style={styles.view}>
+
+
+  const [hasNfc,setHasNFC] = useState<any>(true);
+  useEffect(()=>{
+      async function checkNFC(){
+          const supported = await nfcManager.isSupported()
+          if(supported){
+              await nfcManager.start()
+          }
+          setHasNFC(supported)
+      }
+
+      checkNFC()
+
+  },[])
+  if(hasNfc){
+      return (
+      
+            <View style={styles.view}>
+            <Text>Hello NFC</Text>
       <Text style={textStyles}>React Native thirdweb starter</Text>
       <ConnectWallet />
     </View>
-  );
+          
+        )
+
+  }
+  else if(!hasNfc){
+      return (
+          
+            <View style={styles.view}>
+            <Text>Your device Does Not support NFC</Text>
+      <Text style={textStyles}>React Native thirdweb starter</Text>
+      <ConnectWallet />
+    </View>
+          
+        )
+
+  }
+  return(
+    <>
+    </>
+  )
+
 };
 
 const styles = StyleSheet.create({
@@ -53,3 +92,4 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
